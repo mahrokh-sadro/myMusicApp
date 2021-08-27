e9735f229c4eed052a35041c973bf61a2224bd62
AutoLogon <msadrolodabaee@myseneca.ca>
2021-08-27 03:45:47 -0400

    more css for  play page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
 import React from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
+
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
+
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+
 import Grid from '@material-ui/core/Grid';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import PlayCircleFilledSharpIcon from '@material-ui/icons/PlayCircleFilledSharp';
+
+
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       maxWidth: 345,
       margin: 'auto',
-      // backgroundColor: "#2a2827",
+      backgroundColor: "#2a2827",
       maxHeight: 500,
       paddingBottom: '5%',
       paddingTop: '5%',
-      // marginTop: '1rem',
+      marginTop: '1rem',
       borderRadius: 20,
 
+    }, subColor: {
+      color: '#fff',
+      textAlign: "center",
+      paddingBottom: '10%',
     },
     media: {
       height: 0,
@@ -52,17 +60,13 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default function RecipeReviewCard() {
   const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
 
   return (
     <Card className={classes.root} elevation={4}>
       <CardHeader
-        style={{ color: "red" }}
-        subheader="Playing Now"
+
+        subheader={<Typography className={classes.subColor}>Playing Now</Typography>}
       />
       <CardMedia
         className={classes.media}
@@ -72,20 +76,32 @@ export default function RecipeReviewCard() {
       <CardContent>
         <Grid container>
           <Grid>
-            <Typography variant="body2" color="" component="p" align="left">
-              <h3>singer name</h3>
+            <Typography variant="body2" color="error" component="p" align="left">
+              <h3 >singer name</h3>
               <subtitle1>song name</subtitle1>
             </Typography>
           </Grid>
-          <Grid>
+          <Grid align="right">
 
             <CardActions disableSpacing>
-              <IconButton aria-label="add to favorites">
+              <IconButton aria-label="add to favorites" >
                 <FavoriteIcon />
               </IconButton>
             </CardActions>
 
           </Grid>
+        </Grid>
+        <br />
+        <Grid container paddingTop="12rem" alignContent="center" color="error">
+          <IconButton color="secondary">
+            <SkipPreviousIcon fontSize="large" color="error" />
+          </IconButton>
+          <IconButton >
+            <PlayCircleFilledSharpIcon fontSize="large" color="error" />
+          </IconButton>
+          <IconButton >
+            <SkipNextIcon fontSize="large" />
+          </IconButton>
         </Grid>
 
 
@@ -93,7 +109,7 @@ export default function RecipeReviewCard() {
 
 
       </CardContent>
-      <CardActions disableSpacing>
+      {/* <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
           <FavoriteIcon />
         </IconButton>
@@ -101,7 +117,7 @@ export default function RecipeReviewCard() {
           <ShareIcon />
         </IconButton>
 
-      </CardActions>
+      </CardActions> */}
 
     </Card>
   );
