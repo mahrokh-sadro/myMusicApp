39091178e90751fb44ee288f16efc0d57781003b
AutoLogon <msadrolodabaee@myseneca.ca>
2021-08-27 05:34:00 -0400

    db

--- src/App.js
+++ src/App.js
@@ -1,2 +1,38 @@
 
 
+import { useState } from 'react';
+
+const [songs, setSongs] = useState([
+    {
+        artist: "aaa",
+        title: "song1",
+        img_src: "./images/1.jpg",
+        song_src: "./songs/1.mp3"
+    },
+    {
+        artist: "aaa",
+        title: "song1",
+        img_src: "./images/1.jpg",
+        song_src: "./songs/1.mp3"
+    }, {
+        artist: "bb",
+        title: "song2",
+        img_src: "./images/2.jpg",
+        song_src: "./songs/2.mp3"
+    }, {
+        artist: "cc",
+        title: "song3",
+        img_src: "./images/3.jpg",
+        song_src: "./songs/3.mp3"
+    },
+])
+
+function App() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default App
